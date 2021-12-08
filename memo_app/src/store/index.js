c85bc5d8a30895e302250aab1dc48f23717948b1
napple29 <snwxxx29@gmail.com>
2021-12-08 14:21:19 +0900

    サンプルのメモを削除し、idが1から振られるよう変更

--- memo_app/src/store/index.js
+++ memo_app/src/store/index.js
@@ -5,16 +5,18 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    memos: [
-    {id: 1, body: 'サンプルのメモ'}
-    ]
+    memos: []
   },
   mutations: {
     save (state, memo) {
+      if (state.memos.length){
       const max = state.memos[state.memos.length - 1].id
       memo.id = max + 1
+      } else {
+      memo.id = 1
+      }
       state.memos.push(memo)
-    },
+      },
     update(state, data) {
         const x = state.memos.find(memo => memo.id == data.id);
         x.body = data.body
