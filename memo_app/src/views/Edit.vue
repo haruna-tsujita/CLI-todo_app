f320d7fbce7ab9b160c813a5745152ba4527ea27
napple29 <snwxxx29@gmail.com>
2021-12-08 14:04:20 +0900

    削除機能の実装

--- memo_app/src/views/Edit.vue
+++ memo_app/src/views/Edit.vue
@@ -2,6 +2,7 @@
   <div class="editor">
   <textarea name="memo" v-model="memoBody"></textarea>
   <button @click="save">保存</button>
+  <button class="remove" @click="remove">削除</button>
   </div>
 </template>
 
@@ -31,6 +32,10 @@
           body: this.memoBody
         })
       this.$router.push("/")
+      },
+      remove: function() {
+        this.$store.commit("remove", this.$route.params["id"])
+        this.$router.push("/")
       }
     }
   }
