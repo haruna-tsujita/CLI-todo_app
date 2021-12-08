32700c9ec8bb0dc64ad5ae43b10c09e55c008dba
napple29 <snwxxx29@gmail.com>
2021-12-08 13:54:43 +0900

    メモの編集確定機能の実装

--- memo_app/src/store/index.js
+++ memo_app/src/store/index.js
@@ -14,6 +14,10 @@ export default new Vuex.Store({
       const max = state.memos[state.memos.length - 1].id
       memo.id = max + 1
       state.memos.push(memo)
+    },
+    update(state, data) {
+        const x = state.memos.find(memo => memo.id == data.id);
+        x.body = data.body
     }
   }
 })
