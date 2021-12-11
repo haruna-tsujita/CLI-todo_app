31dcc575b646a445762f6e5ffbb704deed28f475
napple29 <snwxxx29@gmail.com>
2021-12-11 16:39:27 +0900

    /aboutを削除

--- memo_app/src/App.vue
+++ memo_app/src/App.vue
@@ -2,8 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/new">New</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/new">New</router-link>
     </div>
     <router-view name="header"></router-view>
     <router-view/>
